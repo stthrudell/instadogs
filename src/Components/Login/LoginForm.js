837006058b044dab2c3df4c286f32686fb9357bd
Jean Stthrudell <jeanc.lopes@hotmail.com>
2021-02-18 21:24:57 -0300

    Login Form

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -3,10 +3,15 @@ import { Link } from 'react-router-dom'
 
 import Input from '../Forms/Input'
 import Button from '../Forms/Button'
+import Error from '../Helpers/Error'
 
 import useForm from '../../Hooks/useForm'
 
 import { UserContext } from '../../contexts/UserContext'
+
+import styles from './LoginForm.module.css'
+import stylesBtn from '../../Components/Forms/Button.module.css'
+
 
 const LoginForm = () => {
 
@@ -25,15 +30,20 @@ const LoginForm = () => {
         }
     } 
     return (
-        <section>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
+        <section className="animeLeft">
+            <h1 className="title">Login</h1>
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <Input label="Usuário" type="text" name="username" {...username} />
                 <Input label="Senha" type="password" name="password" {...password}/>
-                <Button disabled={loading}>{loading ? 'Carregando...' : 'Entrar'}</Button>
-                {error && <p>{error}</p>}
+                <Button disabled={loading}>{loading ? 'Carregando...' : 'Entrar'}</Button>                
+                <Error error={error} />
             </form>
-            <Link to="/login/criar">Cadastro</Link>
+            <Link className={styles.perdeu} to="/login/perdeu">Perdeu a Senha?</Link>
+            <div className={styles.cadastro}>
+                <h2 className={styles.subtitle}>Cadastre-se</h2>
+                <p>Ainda não possui conta? Cadastre-se no site.</p>
+                <Link className={stylesBtn.button} to="/login/criar">Cadastro</Link>
+            </div>
         </section>
     )
 }

52aaa5ed786cb086aff418fc37cb6652a1853bb9
Jean Stthrudell <jeanc.lopes@hotmail.com>
2021-02-18 22:02:42 -0300

    Login Criar

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -1,10 +1,56 @@
 import React from 'react'
 
+import api from '../../api/api'
+
+import Input from '../Forms/Input'
+import Button from '../Forms/Button'
+import Error from '../Helpers/Error'
+
+import useForm from '../../Hooks/useForm'
+
+import { UserContext } from '../../contexts/UserContext';
+
 const LoginCreate = () => {
+
+    const { userLogin } = React.useContext(UserContext)
+
+    const username = useForm();
+    const email = useForm('email');
+    const password = useForm();
+
+    const [error, setError] = React.useState(null)
+    const [loading, setLoading] = React.useState(false)
+
+    async function handleSubmit(event) {
+        event.preventDefault();        
+        try {
+            setError(null)
+            setLoading(true)
+            api.defaults.headers.post['Content-Type'] = 'application/json'
+            const response = await api.post('api/user', JSON.stringify({
+                username: username.value,
+                email: email.value,
+                password: password.value,
+            }))
+            userLogin({username: username.value, password: password.value})
+        } catch (error) {
+            setError(error.response.data.message)
+        } finally {
+            setLoading(false)
+        }
+    }
+
     return (
-        <div>
-            Login Create
-        </div>
+        <section className="animeLeft">
+            <h1 className="title">Cadastre-se</h1>
+            <form onSubmit={handleSubmit}>
+                <Input label="Usuário" type="text" name="username" {...username} />
+                <Input label="Email" type="email" name="email" {...email} />
+                <Input label="Senha" type="password" name="password" {...password} />
+                <Button disabled={loading}>{loading ? 'Cadastrando...' : 'Cadastrar'}</Button>
+                <Error error={error}/>
+            </form>
+        </section>
     )
 }
 
--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -4,6 +4,10 @@ const types = {
     email: {
         regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         message: 'Email inválido'
+    },
+    password: {
+        regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+        message: 'Deve conter pelo menos 1 número, 1 letra maúscula, 1 minúscula. Com no mínimo 8 caracteres.'
     }
 }
 
